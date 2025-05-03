086e25879748eb7bf95ddf33e0a8d9e367cc1f7e
Matthew Miller <mwm@fastmail.com>
2025-05-03 13:01:59 +0200

    Parse initial messages
    There's not a particularly good way to describe how they are being
    chosen except that: it now parses the full first track of each
    SMF in the `test` directory.  The correstness is yet to be proven,
    but things with which I am familiar appear correct.

--- lib/smf.ex
+++ lib/smf.ex
@@ -1,4 +1,6 @@
 defmodule SMF do
+  alias SMF.{Event, VLQ}
+
   @moduledoc """
   Main interface to Simple Midi Files
   """
@@ -38,10 +40,30 @@ defmodule SMF do
 
   def parse_data(_), do: {:error, "Data does not start with a recognized header"}
 
+  defp parse_tracks(<<>>, acc) do
+    Map.put(acc, :track_data, Enum.reverse(acc.track_data))
+  end
+
   defp parse_tracks(
-         <<"MTrk", len::integer-32, _track_events::bytes-size(len), _rest::binary>>,
+         <<"MTrk", len::integer-32, track_events::bytes-size(len), rest::binary>>,
          acc
        ) do
-    acc
+    parse_tracks(
+      rest,
+      Map.put(acc, :track_data, [parse_events(track_events, []) | acc.track_data])
+    )
+  end
+
+  defp parse_events(<<>>, acc), do: Enum.reverse(acc) |> IO.inspect()
+
+  defp parse_events(data, acc) do
+    case VLQ.decode(data) do
+      {:ok, delta, event} ->
+        {event, rest} = Event.parse(event)
+        parse_events(rest, [{delta, event} | acc])
+
+      err ->
+        err
+    end
   end
 end
---
+++ lib/smf/event.ex
@@ -0,0 +1,155 @@
+defmodule SMF.Event do
+  import Bitwise
+  alias SMF.VLQ
+
+  @moduledoc """
+  Interpreting track events
+  """
+
+  @doc """
+  Parse an event into a useful structure
+  Returns {event, unused data}
+  """
+
+  def parse(
+        <<1::1, 0::1, 0::1, switch::1, channel::integer-4, note::integer-7, 0::1,
+          velocity::integer-7, rest::binary>>
+      ) do
+    type =
+      case switch do
+        0 -> :noteoff
+        1 -> :noteon
+      end
+
+    {%{type: type, channel: channel, note: note, velocity: velocity}, rest}
+  end
+
+  def parse(
+        <<1::1, 0::1, 1::1, 0::1, channel::integer-4, 0::1, note::integer-7, 0::1,
+          pressure::integer-7, rest::binary>>
+      ) do
+    {%{type: :key_pressure, channel: channel, note: note, pressure: pressure}, rest}
+  end
+
+  def parse(
+        <<1::1, 0::1, 1::1, 1::1, channel::integer-4, 0::1, value::integer-7, 0::1,
+          value::integer-7, rest::binary>>
+      ) do
+    # Deal with mode changes on reserved channels
+    chan_msg =
+      case {channel, value} do
+        {122, 0} -> %{type: :channel_mode, local_mode: :off}
+        {122, 127} -> %{type: :channel_mode, local_mode: :on}
+        {123, 0} -> %{type: :channel_mode, all_notes_off: :off}
+        {124, 0} -> %{type: :channel_mode, omni_mode: :on, all_notes: :off}
+        {125, 0} -> %{type: :channel_mode, omni_mode: :off, all_notes: :off}
+        {126, m} -> %{type: :channel_mode, mono_mode: :on, channel_count: m, all_notes: :off}
+        {127, 0} -> %{type: :channel_mode, mono_mode: :off, all_notes: :off}
+        {_, c} -> %{type: :control_change, controller: c}
+      end
+
+    {Map.merge(chan_msg, %{channel: channel}), rest}
+  end
+
+  def parse(
+        <<1::1, 1::1, 0::1, 0::1, channel::integer-4, 0::1, program::integer-7, rest::binary>>
+      ) do
+    {%{type: :program_change, channel: channel, program: program}, rest}
+  end
+
+  def parse(
+        <<1::1, 1::1, 0::1, 1::1, channel::integer-4, 0::1, pressure::integer-7, rest::binary>>
+      ) do
+    {%{type: :channel_pressure, channel: channel, pressure: pressure}, rest}
+  end
+
+  def parse(
+        <<1::1, 1::1, 1::1, 0::1, channel::integer-4, 0::1, lsb::integer-7, 0::1, msb::integer-7,
+          rest::binary>>
+      ) do
+    {%{type: :pitch_wheel_change, channel: channel, value: bsr(msb, 7) + lsb}, rest}
+  end
+
+  def parse(<<0xF0, 0::1, system_id::integer-7, rest::binary>>) do
+    {msgs, actual_rest} = gather_system_exclusive(rest, [])
+    {%{type: :system_exclusive, system_id: system_id, messages: msgs}, actual_rest}
+  end
+
+  def parse(<<0xFF, 0x01, rest::binary>>) do
+    case VLQ.decode(rest) do
+      {:ok, len, more} ->
+        <<text::bytes-size(len), left::binary>> = more
+        {%{type: :text, text: text}, left}
+
+      err ->
+        err
+    end
+  end
+
+  def parse(<<0xFF, 0x2F, 0x00>>), do: {%{type: :end_of_track}, <<>>}
+
+  # TODO: this was poorly documentedin my source
+  # Figure out prporer parsing and meaning of, well, everything
+  def parse(
+        <<0xFF, 0x54, hour::integer, min::integer, sec::integer, fr::integer-16, ff::integer,
+          rest::binary>>
+      ) do
+    {%{type: :smpte_offset, hour: hour, minute: min, second: sec, fr: fr, fractional_frames: ff},
+     rest}
+  end
+
+  def parse(<<0xFF, 0x51, 0x03, tempo::integer-24, rest::binary>>) do
+    {%{type: :set_tempo, μs_per_quater: tempo}, rest}
+  end
+
+  def parse(
+        <<0xFF, 0x58, 0x04, num::integer, dpow::integer, cpc::integer, tpq::integer,
+          rest::binary>>
+      ) do
+    {%{
+       type: :time_signature,
+       numerator: num,
+       denominator: bsl(1, dpow),
+       clocks_per_click: cpc,
+       thirty_per_quarter: tpq
+     }, rest}
+  end
+
+  def parse(<<0xFF, 0x59, 0x02, sf::signed-integer, mi::integer, rest::binary>>) do
+    s_or_f =
+      cond do
+        sf == 0 -> "key of C"
+        sf < 0 -> "#{abs(sf)} flats"
+        sf > 0 -> "#{sf} sharps"
+      end
+
+    maj_min =
+      case mi do
+        0 -> "major"
+        1 -> "minor"
+      end
+
+    {%{type: :key_signature, signature: "#{s_or_f} #{maj_min}"}, rest}
+  end
+
+  def parse(<<0xFF, 0x7F, rest::binary>>) do
+    case VLQ.decode(rest) do
+      {:ok, len, more} ->
+        case more do
+          <<0x00, man_id::integer-16, data::bytes-size(len - 3), left::binary>> ->
+            {%{type: :sequencer_specific, manufacturer_id: man_id, data: data}, left}
+
+          <<man_id::integer, data::bytes-size(len - 1), left::binary>> ->
+            {%{type: :sequencer_specific, manufacturer_id: man_id, data: data}, left}
+        end
+
+      err ->
+        err
+    end
+  end
+
+  defp gather_system_exclusive(<<0xF7, rest::binary>>, acc), do: {Enum.reverse(acc), rest}
+
+  defp gather_system_exclusive(<<msg::integer, rest::binary>>, acc),
+    do: gather_system_exclusive(rest, [msg | acc])
+end
